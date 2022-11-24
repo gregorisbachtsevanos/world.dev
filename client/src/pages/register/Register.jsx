c21f386976ab3fa30846ceed043269a5b5028187
GregorisB <gregorbachts@gmail.com>
2022-11-24 15:38:45 +0200

    bootstrap enabled

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+// import  from "react-bootstrap/Button";
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
@@ -14,71 +17,69 @@ const Register = () => {
     };
 
     return (
-        <form onSubmit={hanldeSubmit}>
-            <div className="mb-3">
-                <label className="form-label">
-                    <span>Firstname</span>
-                    <input
+        <div className="m-auto w-75">
+            <Form className="my-5 mx-auto w-50 row" onSubmit={hanldeSubmit}>
+                <Form.Group className="col-6">
+                    <Form.Label>Firstname</Form.Label>
+                    <Form.Control
                         type="text-control"
-                        className="form-control"
                         onChange={(e) => setFirstname(e.target.value)}
                         required
                         name="firstname"
                     />
-                </label>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">
-                    <span>Lastname</span>
-                    <input
+                </Form.Group>
+                <Form.Group className="col-6">
+                    <Form.Label>Lastname</Form.Label>
+                    <Form.Control
                         type="text-control"
-                        className="form-control"
                         onChange={(e) => setLastname(e.target.value)}
                         required
                         name="lastname"
                     />
-                </label>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">
-                    <span>Username</span>
-                    <input
+                </Form.Group>
+                <Form.Group className="col-6">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
                         type="text-control"
-                        className="form-control"
                         onChange={(e) => setUsername(e.target.value)}
                         required
                         name="username"
                     />
-                </label>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">
-                    <span>Email</span>
-                    <input
+                </Form.Group>
+                <Form.Group className="col-6">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
                         type="text-control"
-                        className="form-control"
                         onChange={(e) => setEmail(e.target.value)}
                         required
                         name="email"
                     />
-                </label>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">
-                    <span>Password</span>
-                    <input
+                </Form.Group>
+                <Form.Group className="col-6">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
                         type="password"
-                        className="form-control"
                         onChange={(e) => setPassword(e.target.value)}
                         required
                         name="password"
                     />
-                </label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-                Submit
-            </button>
-        </form>
+                </Form.Group>
+                <Form.Group className="col-6">
+                    <Form.Label>Repeat your Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        onChange={(e) => setRepeatPassword(e.target.value)}
+                        required
+                        name="repeat_password"
+                    />
+                </Form.Group>
+                <Form.Group className="mt-3">
+                    <Button type="submit" variant="light">
+                        Submit
+                    </Button>
+                </Form.Group>
+            </Form>
+        </div>
     );
 };
 
