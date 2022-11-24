a025b11159571661676fa61f9557d57fffa32f66
GregorisB <gregorbachts@gmail.com>
2022-11-24 15:38:56 +0200

    bootstrap enabled

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from "./App";
-import {store} from "./store";
+import { store } from "./store";
 
 import "./index.css";
 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const hanldeSubmit = async (e) => {
         e.preventDefault();
@@ -14,44 +15,42 @@ const Login = () => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password }),
         });
-        
+
         if (res.status !== 200) {
-            return console.log(res.status)
+            return console.log(res.status);
         }
-        
-        navigate('/')
+
+        navigate("/");
     };
 
     return (
-        <form onSubmit={hanldeSubmit}>
-            <div className="mb-3">
-                <label className="form-label">
+        <Form onSubmit={hanldeSubmit}>
+            <Form.Group className="mb-3">
+                <Form.Label className="form-label">
                     <span>Username</span>
-                    <input
+                    <Form.Control
                         type="text-control"
                         className="form-control"
                         onChange={(e) => setUsername(e.target.value)}
                         required
                         name="username"
                     />
-                </label>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">
+                </Form.Label>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label className="form-label">
                     <span>Password</span>
-                    <input
+                    <Form.Control
                         type="password"
                         className="form-control"
                         onChange={(e) => setPassword(e.target.value)}
                         required
                         name="password"
                     />
-                </label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-                Submit
-            </button>
-        </form>
+                </Form.Label>
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form>
     );
 };
 
