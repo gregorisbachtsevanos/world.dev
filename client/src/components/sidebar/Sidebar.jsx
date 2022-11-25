6a01357f79e49d05d25843f0b7fd30bbd21dbb19
GregorisB <gregorbachts@gmail.com>
2022-11-25 15:43:30 +0200

    ads on side bar

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Nav from "react-bootstrap/Nav";
 import ClickToAction from "./clickToAction/ClickToAction";
 import Categories from "./categories/Categories";
+import Advertisement from "./advertisement/Advertisement";
 
 const Sidebar = () => {
     return (
         <Nav className="flex-column p-2 text-start">
             <ClickToAction />
             <Categories />
-            <Nav.Link>sidebat</Nav.Link>
+            <Advertisement />
         </Nav>
     );
 };
---
+++ client/src/components/sidebar/advertisement/Advertisement.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
+
+const ads = [
+    {
+        image: "https://via.placeholder.com/600x300.png",
+        title: "Calling all early-career developers & coding mentors!",
+        description:
+            "Join CodeNewbie Community: a supportive space for coding newbies to connect & express themselves.",
+    },
+    {
+        image: "https://via.placeholder.com/600x300.png",
+        title: "The Forem Shop is Here!",
+        description: "",
+    },
+];
+
+const Advertisement = () => {
+    return (
+        <>
+            {ads.map((ad) => (
+                <Card key={ad.title} className="my-2 text-start">
+                    <Card.Img variant="top" src={ad.image} />
+                    <Card.Title className="w-75 m-auto">
+                        <Card.Text>{ad.title}</Card.Text>
+                    </Card.Title>
+                    <Card.Body className="w-75 m-auto">
+                        {ad.description && (
+                            <Card.Text>{ad.description}</Card.Text>
+                        )}
+                        <Button variant="link">
+                            <Link to={"#"}>More...</Link>
+                        </Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
+};
+
+export default Advertisement;
