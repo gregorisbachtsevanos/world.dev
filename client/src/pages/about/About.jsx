13a41ec8bbfcd83962e3b06dba85751489805a7c
GregorisB <gregorbachts@gmail.com>
2022-11-26 20:20:03 +0200

    About page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -12,7 +12,11 @@ const About = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Quisquam nemo doloremque quae eum adipisci neque labore
                         voluptates porro, animi at explicabo ab dolor laudantium
-                        odit nisi iure? Tempora, exercitationem optio?
+                        odit nisi iure? Tempora, exercitationem optio? Lorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Maxime earum, consectetur eius obcaecati ratione nobis
+                        porro doloremque deleniti, id odit mollitia sint neque
+                        architecto natus incidunt ipsam sit perferendis itaque.
                     </Card.Text>
                 </Card.Body>
             </Card>
