e4c2fcb2942730a3839ad7d07c51d2a594b5c925
GregorisB <gregorbachts@gmail.com>
2022-11-28 16:48:31 +0200

    redux init

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//* redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+//* app
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import About from './pages/about/About';
@@ -21,34 +26,36 @@ function App() {
   const user = false;
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <Navbar user={user} />
-          <Routes>
-            {/* Public Routers */}
-            <Route element={<PublicRouter user={user} />} >
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Route>
-            {/* Private Routers */}
-            <Route element={<PrivateRouter user={user} />}>
-              <Route path='/new-article' element={<CreateArticle />} />
-              <Route path='/settings' element={<Settings />} />
-              <Route path='/:username' element={<Profile />} />
-            </Route>
-            {/* General Routers */}
-            <Route index element={<Index user={user} />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/sponsorship" element={<Sponsorship />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/guides" element={<Guides />} />
-            <Route path="/privacy" element={<Privacy />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Navbar user={user} />
+            <Routes>
+              {/* Public Routers */}
+              <Route element={<PublicRouter user={user} />} >
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Route>
+              {/* Private Routers */}
+              <Route element={<PrivateRouter user={user} />}>
+                <Route path='/new-article' element={<CreateArticle />} />
+                <Route path='/settings' element={<Settings />} />
+                <Route path='/:username' element={<Profile />} />
+              </Route>
+              {/* General Routers */}
+              <Route index element={<Index user={user} />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/sponsorship" element={<Sponsorship />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/guides" element={<Guides />} />
+              <Route path="/privacy" element={<Privacy />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
